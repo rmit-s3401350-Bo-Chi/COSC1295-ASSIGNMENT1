0670a0a65b5f7e3cf6cc001acda5648639f49ff2
rmit-s3401350-Bo-Chi <31241684+rmit-s3401350-Bo-Chi@users.noreply.github.com>
2018-03-27 13:26:27 +1100

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,315 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Driver {
+
+	private static Set<String> adultFriends = new HashSet<>();
+	private static Set<String> dependentFriends = new HashSet<>();
+	private static List<Person> allPeople = new ArrayList<>();
+	private static List<Adult> allAdult = new ArrayList<>();
+	private static List<Dependent> allChild = new ArrayList<>();
+
+	public Driver() {
+
+	}
+
+
+	/**
+	 * add person to the list
+	 * @param p
+	 */
+
+	public void addPersonToNetwork(Person p) {
+
+		allPeople.add(p);
+
+	}
+	
+	public void addAdultToNetwork(Adult p) {
+
+		allAdult.add(p);
+
+	}
+	
+	public void addChildToNetwork(Dependent p) {
+
+		allChild.add(p);
+
+	}
+
+
+
+	/**
+
+	 * search a person by name
+
+	 * @param name
+
+	 * @return
+
+	 */
+
+	public Person selectPersonByName(String name) {
+
+		for (Person p: allPeople) {
+			if (name.equals(p.getName())) {
+				return p;
+			}
+		}
+		return null;
+	}
+
+	/**
+
+	 * two adults make friend
+
+	 * @param a1
+
+	 * @param a2
+
+	 */
+
+	public void makeFriends(Adult a1, Adult a2) {
+
+		adultFriends.add(a1.getName() + "-" + a2.getName());
+		a1.friends.add(a2);
+		a2.friends.add(a1);
+
+	}
+
+
+
+	/**
+
+	 * two dependent make friend
+
+	 * @param d1
+
+	 * @param d2
+
+	 */
+
+	public void makeFriends(Dependent d1, Dependent d2) {
+
+		if (d1.getAge() < 3) {
+
+			throw new IllegalArgumentException("Dependent " + d1.getName() + " is too young to have a friend.");
+
+		} else if (d2.getAge() < 3) {
+
+			throw new IllegalArgumentException("Dependent " + d2.getName() + " is too young to have a friend.");
+
+		} else if (Math.abs(d2.getAge() - d1.getAge()) > 3) {
+
+			throw new IllegalArgumentException("The age difference between these two young friends cannot be more than 3 years.");
+
+		}
+
+		dependentFriends.add(d1.getName() + "-" + d2.getName());
+		d1.friends.add(d2);
+		d2.friends.add(d1);
+
+	}
+
+
+
+	/**
+
+	 * two person make friend
+
+	 * @param p1
+
+	 * @param p2
+
+	 */
+
+	public void makeFriends(Person p1, Person p2) {
+
+		if (p1 instanceof Adult && p2 instanceof Adult) {
+
+			makeFriends((Adult)p1, (Adult)p2);
+			
+			System.out.println("Adult " + p1.getName()+ " and " + p2.getName() + " are friends now.");
+
+			return;
+
+		}
+
+
+
+		if (p1 instanceof Dependent && p2 instanceof Dependent) {
+
+			makeFriends((Dependent)p1, (Dependent)p2);
+
+			System.out.println("Dependent " + p1.getName()+ " and " + p2.getName() + " are friends now.");
+
+			return;
+
+		}
+
+		System.out.println("Adult cannot make friends with dependent.");
+
+	}
+
+
+
+	/**
+
+	 * check if two adults are friends
+
+	 * @param a1
+
+	 * @param a2
+
+	 * @return
+
+	 */
+
+	public boolean checkFriendship(Adult a1, Adult a2) {
+
+		if (adultFriends.contains(a1.getName() + "-" + a2.getName())) {
+
+			return true;
+
+		}
+
+		if (adultFriends.contains(a2.getName() + "-" + a1.getName())) {
+
+			return true;
+
+		}
+		
+		for(int i=0;i<a1.friends.size();i++) {
+			for(int j=0;j<a2.friends.size();j++) {
+				if(a1.friends.get(i).getName().equals(a2.friends.get(i).getName()))
+					return true;
+			}
+		}
+
+		return false;
+
+	}
+
+
+
+	/**
+
+	 * check if two dependents are friends
+
+	 * @param d1
+
+	 * @param d2
+
+	 * @return
+
+	 */
+
+	public boolean checkFriendship(Dependent d1, Dependent d2) {
+
+		if (dependentFriends.contains(d1.getName() + "-" + d2.getName())) {
+
+			return true;
+
+		}
+
+		if (dependentFriends.contains(d2.getName() + "-" + d1.getName())) {
+
+			return true;
+
+		}
+		
+		for(int i=0;i<d1.friends.size();i++) {
+			for(int j=0;j<d2.friends.size();j++) {
+				if(d1.friends.get(i).getName().equals(d2.friends.get(i).getName()))
+					return true;
+			}
+		}
+
+		return false;
+	}
+
+
+
+	/**
+
+	 * check if two person are friends
+
+	 * @param p1
+
+	 * @param p2
+
+	 * @return
+
+	 */
+
+	public boolean checkFriendship(Person p1, Person p2) {
+
+		if (p1 instanceof Adult && p2 instanceof Adult) {
+
+			return checkFriendship((Adult)p1, (Adult)p2);
+
+		}
+
+
+
+		if (p1 instanceof Dependent && p2 instanceof Dependent) {
+
+			return checkFriendship((Dependent)p1, (Dependent)p2);
+
+		}
+		return false;
+
+	}
+
+
+
+
+
+	/**
+
+	 * get all adults
+
+	 * @return
+
+	 */
+
+	public List<Adult> getAllAdults() {
+
+		return allAdult;
+	}
+
+
+
+	/**
+
+	 * get all dependents
+
+	 * @return
+
+	 */
+
+	public List<Dependent> getAllDependents() {
+
+		return allChild;
+	}
+
+
+
+	/**
+
+	 * get all people
+
+	 * @return
+
+	 */
+
+	public List<Person> getAllPeople() {
+
+		return allPeople;
+
+	}
+
+}
---
+++ Minnet.java
@@ -0,0 +1,182 @@
+import java.util.Scanner;
+
+
+	public class Minnet {
+		public static String MENU = "\nMinnet Menu\n ===================================\n"
+
+				+ "1. Add a person into the network\n"
+				+ "2. Select a person \n"
+				+ "3. List everyone \n"
+				+ "4. Connect two persons\n"
+				+ "5. Are these two direct friend? \n"
+				+ "6. Exit";
+		
+		public static String ENTER = "\n(enter two numbers separated by space)";
+		
+		static String input = "";
+		
+		static Scanner scanner = new Scanner(System.in);
+		
+		static Driver driver = new Driver();
+		
+		public void addPerson() {
+			
+			System.out.println("Enter in the following format: name, age");
+			input = scanner.nextLine().trim();
+
+			String name = input.split(",")[0];
+			int age = Integer.parseInt(input.split(",")[1]);
+			if (age >= 16) {
+				Adult ad = new Adult(name, age);
+				System.out.println("anyother information?");
+				String status = scanner.nextLine();
+				ad.setStatus(status);
+				
+				driver.addPersonToNetwork(ad);
+				driver.addAdultToNetwork(ad);
+				
+			} else {
+				System.out.println("\nChoose two adults as parents" + ENTER);
+				for (int i = 0; i < driver.getAllAdults().size(); i++) {
+					System.out.println(i+1 + ". " + driver.getAllAdults().get(i).getName());
+				}
+				input = scanner.nextLine().trim();
+			
+				int num1 = Integer.parseInt(input.split(" ")[0]);
+				int num2 = Integer.parseInt(input.split(" ")[1]);
+				if (num1 < 0 || num1-1 > driver.getAllAdults().size()-1) {
+					System.out.println("Invalid input!");
+				} else if (num2 < 0 || num2-1 > driver.getAllAdults().size()-1) {
+					System.out.println("Invalid input!");
+				} else {
+					Dependent de = new Dependent(name, age , driver.getAllAdults().get(num1-1),driver.getAllAdults().get(num2-1));
+					de.setparent1(driver.getAllAdults().get(num1-1));
+					de.setparent2(driver.getAllAdults().get(num2-1));
+					
+					driver.getAllAdults().get(num1-1).setSpouse(driver.getAllAdults().get(num2-1));
+					driver.getAllAdults().get(num2-1).setSpouse(driver.getAllAdults().get(num1-1));
+					
+					driver.addPersonToNetwork(de);
+					driver.addChildToNetwork(de);
+				}
+			}
+			
+		}
+		
+		public void selectPerson() {
+			System.out.println("Enter name:");
+			input = scanner.nextLine().trim();
+			Person person = driver.selectPersonByName(input);
+			if (person != null) {
+				if (person instanceof Adult) {
+					System.out.println(person);
+				} else {
+					System.out.println(person);
+				}
+			} else {
+				System.err.println("Cannot find " + input);
+			}
+			
+		}
+		
+		public void connPerson() {
+			
+			for (int i = 0; i < driver.getAllPeople().size(); i++) {
+				System.out.println(i+1 + ". " + driver.getAllPeople().get(i).getName());
+			}
+
+			System.out.println("\nChoose two people to see if they are friends " + ENTER);
+			input = scanner.nextLine().trim();
+			int num1 = Integer.parseInt(input.split(" ")[0]);
+			int num2 = Integer.parseInt(input.split(" ")[1]);
+
+			if (num1 < 0 || num1-1 > driver.getAllPeople().size()-1) {
+
+				System.out.println("Invalid input!");
+
+			} else if (num2 < 0 || num2-1 > driver.getAllPeople().size()-1) {
+
+				System.out.println("Invalid input!");
+
+			} else {
+
+				driver.makeFriends(driver.getAllPeople().get(num1-1), driver.getAllPeople().get(num2-1));
+			}
+
+		}
+		
+		public void choice() {
+			for (int i = 0; i < driver.getAllPeople().size(); i++) {
+				System.out.println(i+1 + ". " + driver.getAllPeople().get(i).getName());
+			}
+
+			System.out.println("\nChoose two people to see if they are friends " + ENTER);
+			input = scanner.nextLine().trim();
+			int num1 = Integer.parseInt(input.split(" ")[0]);
+			int num2 = Integer.parseInt(input.split(" ")[1]);
+			if (num1 < 0 || num1-1 > driver.getAllPeople().size()-1) {
+
+				System.out.println("Invalid input!");
+
+			} else if (num2 < 0 || num2-1 > driver.getAllPeople().size()-1) {
+				System.out.println("Invalid input!");
+			} else {
+				if(driver.checkFriendship(driver.getAllPeople().get(num1-1), driver.getAllPeople().get(num2-1))) {
+					System.out.println(driver.getAllPeople().get(num1-1).getName() + " and " + driver.getAllPeople().get(num2-1).getName() + " are friends");
+				} else {
+					System.out.println("they are not friends!");
+				}
+			}
+		} 
+		
+		public static void main(String[] args) {
+
+			Minnet net = new Minnet();
+			while (true) {
+				
+				try {
+					System.out.println(MENU);
+					input = scanner.nextLine().trim();
+					switch (input) {
+					
+					case "1":
+						net.addPerson();
+						break;
+	
+					case "2":
+	
+						net.selectPerson();
+						break;
+	
+					case "3":
+	
+						for (int i = 0; i < driver.getAllPeople().size(); i++) {
+							System.out.println(i+1 + ". " + driver.getAllPeople().get(i).toString());
+						}
+						break;
+	
+					case "4":
+	
+						net.connPerson();
+						break;
+	
+					case "5":
+	
+						net.choice();
+						break;
+						
+					case "6":
+						System.exit(0);
+						
+					default:
+						System.err.println("Invalid input, please enter 1-7.");
+					}
+					
+				}catch (Exception e) {
+//					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+
